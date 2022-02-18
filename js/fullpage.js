b65cf188347d584e9850fb736b5c36d04bb39f52
Gmail <pitapat7676@gmail.com>
2022-02-18 18:29:20 +0900

    02-18 var02 레이아웃수정중

---
+++ js/fullpage.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+    autoScrolling:true,     // 스크롤 적용할지 안할지
+    scrollHorizontally: true,   // 마우스 휠 또는 트랙패드를 써서 수평으로 미끄러지듯 움직일지
+    fitToSection: true,     // 구역을 모바일 지원(viewport)에 맞출지 말지를 결정
+    scrollingSpeed: 700,   // 스크롤 이동 속도
+    // sectionsColor : ['green', '#CCC', 'blue', 'green'],
+
+
+    navigation: true,           //  작은 원으로 이루어진 Nav
+    navigationPosition: 'left',     // Nav Position
+    navigationTooltips: ['COMPANY', 'B2B BUSINESS', 'B2C BUSINESS', 'PARTNERSHIP'],  // Nav 이름
+    showActiveTooltip:true,     // 말풍선 위젯
+
+})
